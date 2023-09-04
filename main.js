a7427b5a4c78b30fe0f514764dc6167aa181305b
Everton Ramalho <evertonrneto@gmail.com>
2023-09-04 19:07:29 -0300

    another change on directories

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import { renderizarCatalago } from "./src/cartaoProduto";
-import { inicializarCarrinho } from "./src/menuCarrinho";
+import { renderizarCatalago } from "./src/components/cartaoProduto";
+import { inicializarCarrinho } from "./src/components/menuCarrinho";
 
 renderizarCatalago();
 inicializarCarrinho();
